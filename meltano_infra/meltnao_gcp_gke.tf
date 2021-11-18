36d63d5c0fc14355e2007dcd5766820741fa3b30
Ved Prakash <vprakash@gitlab.com>
2021-11-18 11:50:04 +0530

    removed remote state

--- meltano_infra/meltnao_gcp_gke.tf
+++ meltano_infra/meltnao_gcp_gke.tf
@@ -8,13 +8,6 @@ variable "environment" {
     type = string
 }
 
-terraform {
-  backend "gcs" {
-    bucket  = "gitlab-analysis-data-terraform-state"
-    prefix  = "meltano-production/state"
-  }
-}
-
 resource "google_container_cluster" "meltano_cluster" {
     project = "gitlab-analysis"
     location = "us-west1-a"
